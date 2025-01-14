c490da40df8e66df3f5fb8300255982b5c342c4b
notarkhit <notarkhit@gmail.com>
2025-01-14 16:43:57 +0530

    updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,18 @@
 import { generateRandomProfileUrl } from "./avatar.js";
 
+console.log(generateRandomProfileUrl());
+console.log(generateRandomProfileUrl());
+console.log(generateRandomProfileUrl());
+console.log(generateRandomProfileUrl());
+console.log(generateRandomProfileUrl());
+
 function replaceProfileImages() {
  
   const images = document.querySelectorAll('.profile img');
   images.forEach(image => {
-    image.src = generateRandomProfileUrl();
+    const randomUrl = generateRandomProfileUrl();
+    console.log(randomUrl); // Log the random URL
+    image.src = randomUrl;
   });
 }
 
--- scripts/avatar.js
+++ scripts/avatar.js
@@ -2,5 +2,3 @@ function generateRandomProfileUrl() {
   const profileNumber = Math.floor(Math.random() * 70) + 1;
   return `https://i.pravatar.cc/150?img=${profileNumber}`;
 }
-
-const newUrl = generateRandomProfileUrl();
