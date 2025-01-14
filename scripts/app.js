b82344855934de9c221558456006721d1331e1d7
notarkhit <notarkhit@gmail.com>
2025-01-14 16:45:23 +0530

    remove obsolete js module

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,8 @@
-import { generateRandomProfileUrl } from "./avatar.js";
+function generateRandomProfileUrl() {
+  const profileNumber = Math.floor(Math.random() * 70) + 1;
+  return `https://i.pravatar.cc/150?img=${profileNumber}`;
+}
+
 
 console.log(generateRandomProfileUrl());
 console.log(generateRandomProfileUrl());
--- scripts/avatar.js
+++
@@ -1,4 +0,0 @@
-function generateRandomProfileUrl() {
-  const profileNumber = Math.floor(Math.random() * 70) + 1;
-  return `https://i.pravatar.cc/150?img=${profileNumber}`;
-}
